b474a7d7c792c14b2dd4c7333adcb3b739dfea9e
Christo Buschek <crito@30loops.net>
2010-06-21 01:06:43 +0200

    Fixed the template context for home. debug-toolbar added.

--- hitchhiker/views.py
+++ hitchhiker/views.py
@@ -6,7 +6,8 @@ from hitchhiker.models import Itinerary
 def home(request):
     active_trip = Itinerary.objects.filter(active=True)
     if active_trip:
-        return render_to_response('hitchhiker/active_trip.html', {
+        print active_trip[0].destination
+        return render_to_response('hitchhiker/active_trip.html', {   
             'itinerary': active_trip[0]}, 
             context_instance=RequestContext(request))
     else:
