7ea8a266c41a21aae6de4c39559a9d8dc4054d67
Christo Buschek <crito@30loops.net>
2010-10-25 23:04:23 +0200

    Replaced the map with a new one rendered by mapstraction.

--- settings.py
+++ settings.py
@@ -58,7 +58,7 @@ MEDIA_ROOT = '/srv/Envs/mariazendre.org/hitchhiking/media/'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = 'http://mariazendre.org'
+MEDIA_URL = 'http://media.mariazendre.org'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
