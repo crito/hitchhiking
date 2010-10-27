7e7257bcb949c4974ec7785c7ed112da92ba68f3
Christo Buschek <crito@30loops.net>
2010-10-27 12:53:33 +0200

    Blog displays post details and lists all posts.

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls.defaults import *
 from django.views.generic import list_detail
 from blog.models import Post
+from blog import views
 
 post_list = {
-    'queryset': Post.objects.all(),
+    'queryset': Post.objects.filter(status=2),
     }
 
 urlpatterns = patterns('',
-    (r'^$', list_detail.object_list, post_list),
+    (r'^$', list_detail.object_list, post_list, 'post_list'),
+    #(r'(?P<slug>\w+)/$', {}, views.post_detail, 'post_detail'),
+    (r'(?P<slug>[\w-]+)/$', list_detail.object_detail, dict(post_list, slug_field="slug"), 'post_detail'),
 )
 
--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.shortcuts import render_to_response, redirect, get_object_or_404
+from django.views.generic import list_detail
+
+def post_detail(request, slug):
+    '''Display one post.'''
+    posts = Post.objects.all()
+
+    return list_detail.object_detail(request, queryset=post, template_name='blog/post_detail.html', slug=slug)
